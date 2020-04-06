da9178f462d0cd37c8b86fae66155afb8a116a60
Coursera Learner <nobody@nowhere.nul>
2020-04-06 04:31:25 +0000

    finished

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+ 
+/*
 
-/* 
  * Determines if coord is in range between
  * offset (INCLUSIVE) and offset + size (EXCLUSIVE)
  */
-int isInRange(int coord, int offset, int size) {
+//int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
   // else, return 0
-  return 0;
-}
+//  return 0;
+//}
 
 /*
  * Determines if coord is at border of offset or
  * offset + size
  */
-int isAtBorder(int coord, int offset, int size) {
+//int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
   // return 1, else return 0
-  return 0;
+//  return 0;
+//}
+
+int w=0;
+int h=0;
+int max(int a, int b){
+  if (a>b){
+    return a;}
+  else {return b;}
 }
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
@@ -30,6 +39,20 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
   //count from 0 to h. Call the number you count with y
 
     //count from 0 to w. Call the number you count with x
+  //compute the max of size1 and (x_offset + size2).  Call this w
+  w=max(size1,(x_offset+size2));
+  h=max(size1, (y_offset+size2));
+  
+  //compute the max of size1 and (y_offset + size2).  Call this 
+  //count from 0 to h. Call the number you count with y
+ int y;
+ for(y=0;y<h;++y)
+   {
+    
+    //count from 0 to w. Call the number you count with x
+     int x;
+     for(x=0;x<w;++x)
+       {
 
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
@@ -38,7 +61,19 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-
+	if(((x>=x_offset&&x<x_offset+size2)&&(y==y_offset||y==y_offset+size2-1))||((y>y_offset&&y<y_offset+size2)&&(x==x_offset||x==x_offset+size2-1)))
+    {
+      printf("*");//if so, print a*
+    }
+	else if(((x<size1&&(y==0||y==size1-1))||(y<size1&&(x==0||x==size1-1))))
+	  {
+	    printf("#");
+	  }
+	else{
+	printf(" ");
+       }
+       }
+     printf("\n");
       //if not,
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
@@ -49,5 +84,6 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //else print a space
     //when you finish counting x from 0 to w, 
     //print a newline
-
+   }
 }
+
