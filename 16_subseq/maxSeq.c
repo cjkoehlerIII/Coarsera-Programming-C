d381fb647dfa6285fc3dc201bb05806a053c430b
Coursera Learner <nobody@nowhere.nul>
2020-06-04 04:58:19 +0000

    done?

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -48,7 +48,7 @@ size_t maxSeq(int *array, size_t n){
       if(current_seq>max_seq){
 	max_seq=current_seq;};
   
-    };
+    }
 
     if(max_seq>super_seq){
       super_seq=max_seq;}
@@ -56,7 +56,7 @@ size_t maxSeq(int *array, size_t n){
 
   
    return super_seq;
-};
+}
   
 
     
