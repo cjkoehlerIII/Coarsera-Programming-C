8df770ff209341cc33bd33370be2e7733e8ae0e9
U-DESKTOP-PBA7RJ2\cjkoe <cjkoehlerphd@gmail.com>
2020-06-07 23:42:37 -0500

    finished

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -3,58 +3,46 @@
 
 size_t maxSeq(int *array, size_t n){
 
-  size_t current_seq=1;
+  size_t current_seq=0;
   size_t max_seq=0;
   size_t super_seq=0;
-  int i=0;
-  int j=0;
+  size_t i=0;
   
-  int *p=&array[0];
   
-     
-  if(*(p+i)=='\0'){
+  int *p=array;
+
+  if(n<1){
     super_seq=0;
-     return super_seq=0;};
-
-  if(n==1 || n==0){
-    super_seq=1;
-     return super_seq=1;};
-
-  if(array[0]==0 && n==1){
-    super_seq=1;
-    return super_seq=0;};
-
-  for(j=0;j<n;j++){
-    current_seq=1;
-    max_seq=0;
-     
-    for(i=j;i<n;i++){
-
-      if(*(p+i)<0){
-	continue;}
-
-      if(*(p+i+1)==*(p+i)){
-	break;}
-
-      if(*(p+i+1)>*(p+i)){
-	current_seq=current_seq+1;};
-
-      if(current_seq>max_seq){
-	max_seq=current_seq;};
-
-      if(*(p+i+1)<*(p+i)){
-	break;};
+    return super_seq=0;
+  }
   
-      if(current_seq>max_seq){
-	max_seq=current_seq;};
-  
-    };
+  if(array[0]==1 && n==1){
+    super_seq=1;
+    return super_seq;}
+       
+  if(array[0]=='\0'){
+    super_seq=1;
+    return super_seq;}
+
+ 
+  for(i=0;i<n;i++){
+
+    for(size_t j =0;*(p+i+1)>*(p+i)&&*(p+i)>=0 && i<n; j++){
+      current_seq=j+2;
+      i++;
+    }
+
+    if(current_seq>max_seq)
+      {max_seq=current_seq;}
+ 
+    
     if(max_seq>super_seq){
-      super_seq=max_seq;}
+	super_seq=max_seq;};
+      
   };
 
    return super_seq;
-};
+  };
   
 
     
